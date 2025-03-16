bf4ebe00ccdfc7548343c24031cd06eb45ba81b8
KobusBooyens <kobusb@runninghill.co.za>
2025-03-16 10:35:29 +0200

    Added reservation form and list and functionality

--- src/app/reservation-form/reservation-form.component.ts
+++ src/app/reservation-form/reservation-form.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ReservationService } from '../reservation/reservation.service';
+import { Reservation } from '../models/reservation';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-reservation-form',
   templateUrl: './reservation-form.component.html',
   styleUrls: ['./reservation-form.component.css']
 })
-export class ReservationFormComponent {
 
+export class ReservationFormComponent implements OnInit {
+  reservationForm: FormGroup = new FormGroup({});
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private reservationService: ReservationService,
+    private router: Router
+  ) { }
+
+  ngOnInit() {
+    this.reservationForm = this.formBuilder.group({
+      checkInDate: ['', Validators.required],
+      checkOutDate: ['', Validators.required],
+      guestName: ['', Validators.required],
+      guestEmail: ['', [Validators.required, Validators.email]],
+      roomNumber: ['', [Validators.required, Validators.min(1), Validators.max(38)]]
+    });
+  }
+
+  onSubmit() {
+    console.log(this.reservationForm.valid);
+    if (this.reservationForm.valid) {
+      this.reservationService.addReservation(this.reservationForm.value);
+      this.router.navigate(['/list']);
+      this.reservationForm.reset();
+    }
+  }
 }
--- src/app/reservation/reservation.service.ts
+++ src/app/reservation/reservation.service.ts
@@ -1,9 +1,58 @@
 import { Injectable } from '@angular/core';
+import { Reservation } from '../models/reservation';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class ReservationService {
 
-  constructor() { }
+  private reservations: Reservation[] = [];
+
+  constructor() {
+    let reservation = this.getFromLocalStorage();
+    this.reservations = reservation;
+  }
+
+  getReservations(): Reservation[] {
+    return this.reservations;
+  }
+
+  getReservationById(id: string): Reservation | undefined {
+    return this.reservations.find(res => res.id === id);
+  }
+
+  addReservation(reservation: Reservation) {
+    console.log("add reservation", reservation);
+    reservation.id = new Date().getTime().toString();
+    this.reservations.push(reservation);
+    this.updateLocalStorage();
+  }
+
+  deleteReservation(reservation: Reservation) {
+    const index = this.reservations.indexOf(reservation);
+    if (index !== -1) {
+      this.reservations.splice(index, 1);
+    }
+    this.updateLocalStorage();
+  }
+
+  updateReservation(reservation: Reservation) {
+    const index = this.reservations.indexOf(reservation);
+
+    this.reservations[index] = reservation;
+    this.updateLocalStorage();
+  }
+
+  updateLocalStorage() {
+    localStorage.setItem('reservations', JSON.stringify(this.reservations));
+  }
+
+  getFromLocalStorage(): Reservation[] {
+    const data = localStorage.getItem('reservations');
+    if (data) {
+      return JSON.parse(data);
+    }
+    return [];
+  }
 }
